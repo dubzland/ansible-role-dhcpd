c381be0bd9a0257dfba6f650be1d12358908cc4d
Josh Williams <vmizzle@gmail.com>
2019-10-20 09:37:17 -0500

    Initial commit

---
+++ tests/run_tests.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+ansible-playbook -i /usr/src/dubzland-dhcpd/tests/inventory.yml /usr/src/dubzland-dhcpd/tests/test_primary.yml
+# ansible-playbook -i /usr/src/dubzland-dhcpd/tests/inventory.yml /usr/src/dubzland-dhcpd/tests/test_secondary.yml
